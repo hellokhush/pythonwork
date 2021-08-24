aa1b3f01c9f9e6e4d692cc19e44cf68e88dd0092
Khushboo <itkhush17@gmail.com>
2021-08-24 17:57:53 +0530

    Create list_ec2_defaultvpc.py

---
+++ list_ec2_defaultvpc.py
@@ -0,0 +1,26 @@
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
+region = 'us-west-2'
+client = boto3.client('ec2', region_name = region)
+output = {}
+try:
+            
+    response = client.describe_vpcs(Filters=[{'Name': 'isDefault', 'Values': ['true']}])
+    if not len(response['Vpcs']):
+        vpc_id = None
+    else:
+        vpc_id = response.get('Vpcs', [{}])[0].get('VpcId', '')
+        
+    ec2 = boto3.resource('ec2', region_name = region)       
+    vpc = ec2.Vpc(vpc_id)
+#print(vpc)
+
+    for instance in vpc.instances.all():
+        for tag in instance.tags:
+            if tag['Key'] == 'Name':  
+                print ("\t{0}\t\t {1}".format('Id', 'Instance_Name'))  
+                
+                print (" {0}\t {1}".format(instance.id, tag['Value']))
+except ValueError as err:
+    print(err.args )
+         
